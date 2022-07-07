6c9560a95b92eeac66226a61d58d25e754030278
Sergey Miniaylo <po4tovuj@gmail.com>
2022-07-07 20:05:19 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+      deserunt quidem inventore illum saepe, veritatis veniam odit totam
+      eligendi dolores magnam fuga atque. Nostrum illum, ratione amet ea dolore
+      ipsam accusantium! Iste ipsam quas provident cupiditate iusto sint
+      assumenda optio officia eius facere, nesciunt id pariatur, accusamus
+      laborum sunt laudantium vero quibusdam repellendus voluptates rerum! Sint
+      quis cumque esse dolor minima animi, ad fuga odio tempore explicabo iste
+      nihil temporibus harum porro earum minus. Saepe, rem officia officiis
+      error mollitia, reiciendis quis omnis eveniet consequatur, hic possimus
+      fuga facilis atque provident vero. Labore, porro optio! Et quibusdam
+      eligendi deleniti fugiat, cupiditate illo culpa natus explicabo
+      exercitationem itaque a fuga est, eos molestias. Distinctio, et quibusdam.
+      Nesciunt officiis reiciendis laborum eius, iste placeat excepturi animi
+      debitis nemo beatae amet voluptate quibusdam molestias modi tenetur autem
+      dolorem obcaecati voluptas quidem, porro vel recusandae, rem magni. Error
+      asperiores nemo ab eum perferendis ipsum dolorum facilis quo veritatis
+      mollitia tempora sequi sint reiciendis, necessitatibus dolor! Facere
+      placeat sit dicta amet suscipit voluptatum nam, magnam impedit recusandae
+      magni nemo, corporis animi accusantium? Itaque explicabo quam suscipit
+      nisi impedit facere ab tempora ipsa saepe reprehenderit ut ducimus totam
+      harum pariatur at delectus, non odio corporis doloribus quas repudiandae
+      ullam aliquid excepturi. Voluptatum, aliquid, doloremque nihil odit quas,
+      officia expedita eum accusantium impedit sunt facere laudantium ab eos.
+      Iusto natus aut optio aspernatur, quas cum molestias neque iure quis! Illo
+      velit illum ad dolore, aliquid vel itaque veniam possimus non aspernatur
+      suscipit aliquam optio dicta, ea maiores voluptates commodi ipsa
+      consectetur quasi ut similique reiciendis. Repellat amet quaerat
+      repudiandae architecto at et? Iure obcaecati, in, quos rem, ratione
+      impedit suscipit saepe vero blanditiis nobis adipisci nam similique nulla
+      quam molestias maxime? Sed distinctio molestiae facilis est enim quaerat,
+      cum sapiente quas nihil repellat, recusandae dignissimos rem deserunt
+      velit quos nesciunt omnis fuga minima doloremque asperiores quod iusto
+      veniam ipsam? Voluptas provident perferendis laudantium, quisquam officia
+      ipsum sed voluptate beatae excepturi quasi inventore doloribus quae,
+      incidunt blanditiis iste. Perspiciatis animi consequatur sed voluptates
+      eligendi libero delectus exercitationem repellendus illo vel omnis
+      aspernatur similique dicta rem modi esse, voluptas, nam iusto repellat!
+      Reiciendis inventore dolore eligendi est minima maxime nobis enim eius
+      quo, ratione aperiam voluptatibus porro, necessitatibus maiores, vero
+      repudiandae facilis possimus illo. Sit consectetur vero cum porro at
+      perferendis corporis saepe soluta eaque fugit necessitatibus quo,
+      laboriosam labore voluptatem doloremque, hic numquam neque ipsam sunt
+      velit, asperiores facere repudiandae possimus sint. Libero possimus
+      corrupti in officia alias iusto rerum delectus quis, perspiciatis ut iure
+      at! Ullam expedita accusamus et repellendus quaerat consectetur veritatis
+      fuga vitae enim quos omnis, veniam dicta. Cupiditate quod excepturi nihil
+      possimus ea ullam ut aut suscipit perspiciatis quas vitae explicabo
+      officia enim tempore vero accusamus, eum quo amet? Officiis commodi atque
+      magni et a libero ad at numquam iusto. Facilis exercitationem culpa
+      excepturi odit recusandae dolorum, neque, quasi officia minima natus
+      blanditiis cumque vel explicabo quis? Voluptates beatae repellendus sequi
+      enim accusantium cumque, iure ea excepturi. Architecto perspiciatis
+      tenetur odit amet rem iste.
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+<style lang="scss" scoped></style>
